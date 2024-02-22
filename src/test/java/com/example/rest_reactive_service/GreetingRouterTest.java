7b8954b0c895e63e7ffbb1c2a36a6db069001c50
julliet <khodyrevajulya@yandex.ru>
2024-02-22 11:43:42 +0300

    debug test.java

--- src/test/java/com/example/rest_reactive_service/GreetingRouterTest.java
+++ src/test/java/com/example/rest_reactive_service/GreetingRouterTest.java
@@ -9,7 +9,6 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(SpringExtension.class)
-//  We create a `@SpringBootTest`, starting an actual server on a `RANDOM_PORT`
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GreetingRouterTest {
     @Autowired
@@ -18,11 +17,9 @@ public class GreetingRouterTest {
     @Test
     public void testHello() {
         webTestClient
-                // Create a GET request to test an endpoint
                 .get().uri("/hello")
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
-                // and use the dedicated DSL to test assertions against the response
                 .expectStatus().isOk()
                 .expectBody(Greeting.class).value(greeting -> {
                     assertThat(greeting.getMessage()).isEqualTo("Hello, Spring!");
