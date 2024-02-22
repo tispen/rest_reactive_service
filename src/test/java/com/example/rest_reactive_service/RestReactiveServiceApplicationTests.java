fd635de0fc872eb174dc90e7530a2e08a6b881a1
julliet <khodyrevajulya@yandex.ru>
2024-02-22 11:39:14 +0300

    init

---
+++ src/test/java/com/example/rest_reactive_service/RestReactiveServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest_reactive_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestReactiveServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
